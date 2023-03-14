a8979838f8eae90d6c686870c9fb7a455a44c73e
povilas-luk <povilas.lukasevicius@gmail.com>
2023-03-14 20:10:43 +0200

    added reservations

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,7 @@ import './Hero.css'
 import Reservation from './Reservation'
 
 const Hero = () => {
+  const [openReservation, setOpenReservation] = React.useState(false)
   return (
     <section>
       <div className='hero'>
@@ -10,11 +11,11 @@ const Hero = () => {
         <h1 className='p-markazi'>Little Lemon</h1>
         <h3 className='p-markazi'>Chicago</h3>
         <p className='p-karla'>We are family owned Mediterranean restaurant, focused on traditional recipies served with a modern twist.</p>
-        <button className='p-markazi'>Reserve a Table</button>
+        <button className='p-markazi' onClick={() => {setOpenReservation(true)}}>Reserve a Table</button>
       </div>
       <img src="images/restauranfood.jpg" alt="restaurant food image" />
       </div>
-      <Reservation />
+      {openReservation && <Reservation closeReservation={setOpenReservation}/>}
     </section>
   )
 }
--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,21 +1,63 @@
 import React from 'react'
 import "./Reservation.css"
+import {useForm} from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup";
 
-const Reservation = () => {
+const schema = yup.object({
+    name: yup.string().required("Name is a required!"),
+    email: yup.string().required("Email is a required!").email("Email is not valid!"),
+    people: yup.number().min(1, "1 person minimum!").required("Please specify number of people!"),
+    date: yup.string().required("Select date and time!"),
+})
+
+const Reservation = ({closeReservation}) => {
+
+const { handleSubmit, register, formState: { errors } } = useForm({
+      resolver: yupResolver(schema)
+  })
+
+  const formSubmit = (data) => {
+      closeReservation(false)
+  }
 
   return (
-    <div className='reservation-background'>
-      <div className='reservation-container'>
-        <button> X </button>
-        <form >
-          <label htmlFor="name">Name</label>
-          <input id='name' type="text" placeholder='Name..' />
-          <input type="text" placeholder='email@email.com' />
-          <input type="text" placeholder='password123' />
-          <input type="submit" />
+    <div className='reservation'>
+      <div className='background'></div>
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <fieldset>
+            <img src="images/x-symbol.svg" alt="exit" onClick={() => {closeReservation(false)}}/>
+            <h1>Reserve a Table</h1>
+            <div className="field">
+              <input type="text" placeholder="Name" {...register("name")} />
+              <span className="error-message">{errors.name?.message}</span>
+            </div>
+            <div className="field">
+              <input type="text" placeholder="Email" {...register("email")}/>
+              <span className="error-message">{errors.email?.message}</span>
+            </div>
+            <div className="field">
+              <div className="options">
+                <select name="occasion" {...register("occasion")}>
+                  <option value="select">Occasion</option>
+                  <option value="birthday">Birthday</option>
+                  <option value="anniversary">Anniversary</option>
+                  <option value="engagement">Engagement</option>
+                </select>
+              </div>
+            </div>
+            <div className="field">
+              <input type="number" placeholder="People" {...register("people")}/>
+              <span className="error-message">{errors.people?.message}</span>
+            </div>
+            <div className="field">
+              <input type="datetime-local" {...register("date")} />
+              <span className="error-message">{errors.date?.message}</span>
+            </div>
+            <button className="reserve-btn" type="submit">Reserve</button>
+          </fieldset>
         </form>
       </div>
-    </div>
   )
 }
 
